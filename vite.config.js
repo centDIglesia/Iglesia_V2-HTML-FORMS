72e1b129d50d640eb2d3f6dc3bf670e4205b2630
centDiglesia <vincent.iglesia07@gmail.com>
2024-04-08 10:23:28 +0800

    footerdonr

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import VitePluginPages from 'vite-plugin-gh-pages';
 export default defineConfig({
   base: '/Iglesia_V2-HTML-FORMS/', // Set the base path for GitHub Pages
   plugins: [
-    // other plugins...
-    VitePluginPages(),
   ],
 });
