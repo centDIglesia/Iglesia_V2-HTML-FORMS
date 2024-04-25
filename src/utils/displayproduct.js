01d032fb76fa690849b65a3a9e623a077d626e7c
centDiglesia <vincent.iglesia07@gmail.com>
2024-04-25 20:15:26 +0800

    em

--- src/utils/displayproduct.js
+++ src/utils/displayproduct.js
@@ -3,21 +3,23 @@ import { products } from "../../data/products";
 export default function displayProducts() {
   let productHTML = '';
 
-  products.forEach((product) => {
-    productHTML += `
-    <div class="menu__card">
-      <div class="card__image-container">
-        <img src="${product.image}" alt="" class="card__image" />
+  products
+    .filter(product => product.type === 'customize') 
+    .forEach((product) => {
+      productHTML += `
+      <div class="menu__card">
+        <div class="card__image-container">
+          <img src="${product.image}" alt="" class="card__image" />
+        </div>
+        <div class="card__content">
+          <h3 class="card__name">${product.name}</h3>
+          <p class="card__price">${product.priceCents}</p>
+          <p class="card__description">${product.description}</p>
+          <a class="card__btn btn" href="#order__page" data-product-id="${product.id}">Add to cart</a>
+        </div>
       </div>
-      <div class="card__content">
-        <h3 class="card__name">${product.name}</h3>
-        <p class="card__price">${product.priceCents}</p>
-        <p class="card__description">${product.description}</p>
-        <a class="card__btn btn" href="#order__page" data-product-id="${product.id}">Add to cart</a>
-      </div>
-    </div>
-  `;
-  });
+    `;
+    });
 
   document.querySelector(".menu__products-flex").innerHTML = productHTML;
 }
